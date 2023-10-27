88effee1b12afa17dd75a2749e1d7581a4d0ed70
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-10-27 15:49:20 -0500

    Video render carousel and image

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import imagen1 from "../assets/IMG_1214 2.jpg";
+import imagen2 from "../assets/IMG_1222 2.jpg";
+import imagen3 from "../assets/IMG_1333 2.jpg";
 
 function ImageCarousel() {
   return (
-    <Carousel>
+    <Carousel autoPlay={true} interval={2500} showStatus={false}  showArrows={false}>
       <div>
-        <img src="image1.jpg" alt="Image 1" />
-        <p className="legend">Descripción de la imagen 1</p>
+        <img src={imagen1} alt="Image 1" />
       </div>
       <div>
-        <img src="image2.jpg" alt="Image 2" />
-        <p className="legend">Descripción de la imagen 2</p>
+        <img src={imagen2} alt="Image 2" />
       </div>
       <div>
-        <img src="image3.jpg" alt="Image 3" />
-        <p className="legend">Descripción de la imagen 3</p>
+        <img src={imagen3} alt="Image 3" />
       </div>
-      {/* Agrega más imágenes según sea necesario */}
     </Carousel>
   );
 }
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import ImageCarousel from "../components/Carousel";
-import VideoPlayer from "../components/VideoPlayer";
 import styles from "./Landing.module.css";
+import videoLanding from "../assets/videoLanding.MP4";
+import imagen1 from "../assets/IMG_1214 22.jpg";
 
 const Landing = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <video src={videoLanding} autoPlay loop muted />
+      </div>
 
-    return (
-        <div>
-            <div className={styles.videoDiv}>
-                <VideoPlayer    
-                    videoSrc="/video/My project.MP4"
-                    width={800}
-                    height={450}
-                    />
-            </div>
-            este es el carrousel nene
-            <ImageCarousel />
-            hola nene
+      <div className={styles.imageContainer}>
+        <div className={styles.textContainer}>
+          <div className={styles.bienvenidos}>BIENVENIDOS A NUESTRA CASA</div>
         </div>
-    )
-}
+        <img src={imagen1} alt="imagen1" />
+      </div>
+
+      <div>
+        <ImageCarousel />
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
 
+
+
+ 
