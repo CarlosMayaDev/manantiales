3b1c483fc22f67a5bf0a7bd2570cd04ff7655e5f
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-11-28 11:32:26 -0500

    Conocenos view completed

--- src/views/Conocenos.jsx
+++ src/views/Conocenos.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import styles from "./Conocenos.module.css"
+import styles from "./Conocenos.module.css";
+import imagenEvangelizar from "../assets/378317721_630360289263064_4111713357532053760_n.jpg";
+import imagenAfirmar from "../assets/299980236_6156034497759389_2543737134630714748_n.jpg"
+import imagenDiscipular from "../assets/discipular.jpg";
+import imagenEnviar from "../assets/enviar.jpg";
 
 const Conocenos = () => {
     return (
@@ -22,16 +26,39 @@ const Conocenos = () => {
                 Siguiendo la enseñanza de Jesús, tal como se expresa en Marcos 16:15, la Comunidad de Manantiales de Amor y Poder, como parte activa del cuerpo de Cristo, se embarca con entusiasmo en la misión de la Gran Comisión. Nos esforzamos por impulsar cambios significativos en las familias, buscando crear una sociedad fundamentada en principios y valores inspirados en la sabiduría de la Palabra de Dios. ¡Somos agentes de transformación con un propósito claro y relevante!
                 </p>            
             </div>
-            <div className={styles.misionVision}>
-                <div className={styles.mision}>
-                    <h3 className={styles.h3}>Misión</h3>
-                    <p className={styles.h4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            
+            <div className={styles.evangelizarContainer}>
+                <div>
+                    <h3 className={styles.h3}>Evangelizar</h3>
+                    <p className={styles.h4}>Compartir el evangelio implica llevar el mensaje positivo del Reino de Dios a quienes enfrentan desafíos espirituales, emocionales y físicos, alcanzando a todas las personas en todo el mundo.</p>
                 </div>
-                <div className={styles.mision}>
-                    <h3 className={styles.h3}>Visión</h3>
-                    <p className={styles.h4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+            </div>
+
+            <div className={styles.evangelizarContainer}>
+                <img src={imagenAfirmar} alt="FotoAfirmar" />
+                <div>
+                    <h3 className={styles.h3}>Afirmar</h3>
+                    <p className={styles.h4}>Establecer el objetivo de transformar a cada creyente en un discípulo y líder sólido en el Reino de Dios, basándolo en los valores y la doctrina bíblica, con el propósito de renovar su mentalidad y lograr una transformación significativa en su vida.</p>
                 </div>
             </div>
+
+            <div className={styles.evangelizarContainer}>
+                <div>
+                    <h3 className={styles.h3}>Discipular</h3>
+                    <p className={styles.h4}>Entrenar, capacitar, perfeccionar y apoyar a cada discípulo para que descubra y cumpla el propósito, el llamado y la voluntad de Dios en su vida. Esto implica ayudarle a comprender su identidad como hijo, con derechos, privilegios, herencia y responsabilidades en el Reino de Dios.</p>
+                </div>
+                <img src={imagenDiscipular} alt="FotoDiscipular" />
+            </div>
+
+            <div className={styles.evangelizarContainer}>
+                <img src={imagenEnviar} alt="FotoEnviar" />
+                <div>
+                    <h3 className={styles.h3}>Enviar</h3>
+                    <p className={styles.h4}>Empoderar a cada discípulo para que cumpla la voluntad de Dios y alcance su destino, llevando consigo el Reino de Dios a donde quiera que vaya. Esto implica predicar y enseñar la palabra con milagros y prodigios, con el propósito de impactar positivamente en la sociedad. Todo esto se realiza con la intención de transmitir un legado de bendición, prosperidad y justicia a las generaciones futuras.</p>
+                </div>
+            </div>
+
             <div className={styles.horario}>
                 <h3 className={styles.h3}>Horarios de servicio</h3>
                 <p className={styles.h4}>Viernes 6:30 pm, Domingo 8:00 am y 10:00 am.</p>
