6d9efa6119aa49d714beb776d2e614dc60263909
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-12-01 11:57:00 -0500

    Ministerios view

--- src/views/Ministerios.jsx
+++ src/views/Ministerios.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../views/Ministerios.module.css"
-import imagenEvangelizar from "../assets/enviar.jpg"
+import imagenEvangelizar from "../assets/97NmG-uEEaeP42qtLSrGid8gB972nlIc9BufKXiC_JU=_plaintext_638363737802768982.jpg"
 
 const Ministerios = () => {
     return (
@@ -14,19 +14,104 @@ const Ministerios = () => {
             </div>
 
             <div className={styles.ministeriosContainer}>
-                 <h1 className={styles.h7}>Eje adolescentes – ManantialEs Familia</h1>
+                 <h1 className={styles.h7}>Ministerio Infantil – Conquistadores del Reino</h1>
                 <p className={styles.p}>
                     Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
-                    l equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
                 </p>
             </div>
-            <div className={styles.evangelizarContainer}>
+            <div className={styles.ministeriosDescripcionContainer}>
                 <div>
-                    <h3 className={styles.h3}>Evangelizar</h3>
-                    <p className={styles.h4}>Compartir el evangelio implica llevar el mensaje positivo del Reino de Dios a quienes enfrentan desafíos espirituales, emocionales y físicos, alcanzando a todas las personas en todo el mundo.</p>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
                 </div>
                 <img src={imagenEvangelizar} alt="FotoEvangelizar" />
             </div>
+
+        
+            <div className={styles.ministeriosContainer}>
+                 <h1 className={styles.h7}>Eje adolescentes – ManantialEs Familia</h1>
+                <p className={styles.p}>
+                    Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                </p>
+            </div>
+            <div className={styles.ministeriosDescripcionContainer2}>
+            <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+                <div>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
+                </div>
+            </div>
+
+            <div className={styles.ministeriosContainer}>
+                 <h1 className={styles.h7}>Ministerio de Adoración</h1>
+                <p className={styles.p}>
+                    Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                </p>
+            </div>
+            <div className={styles.ministeriosDescripcionContainer}>
+                <div>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
+                </div>
+                <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+            </div>
+
+        
+            <div className={styles.ministeriosContainer}>
+                 <h1 className={styles.h7}>STAFF</h1>
+                <p className={styles.p}>
+                    Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                </p>
+            </div>
+            <div className={styles.ministeriosDescripcionContainer2}>
+            <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+                <div>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
+                </div>
+            </div>
+
+            <div className={styles.ministeriosContainer}>
+                 <h1 className={styles.h7}>Red de Intercesores</h1>
+                <p className={styles.p}>
+                    Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                </p>
+            </div>
+            <div className={styles.ministeriosDescripcionContainer}>
+                <div>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
+                </div>
+                <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+            </div>
+
+        
+            <div className={styles.ministeriosContainer}>
+                 <h1 className={styles.h7}>Multimedia</h1>
+                <p className={styles.p}>
+                    Somos un equipo de trabajo que busca acompañar al adolescente en su paso hacia la juventud, esto desde una perspectiva bíblica y Cristo céntrica. <br />
+                    El equipo focaliza sus esfuerzos en generar un ambiente de confianza, seguridad, crecimiento y amor: Una familia en la cual el adolescente puede aprender junto a otros que estan pasando por esta etapa.
+                </p>
+            </div>
+            <div className={styles.ministeriosDescripcionContainer2}>
+            <img src={imagenEvangelizar} alt="FotoEvangelizar" />
+                <div>
+                    <h3 className={styles.h3}>Que buscamos?</h3>
+                    <p className={styles.p}>Que cada asistente reconozca su necesidad de Jesús como redentor, esto al concientizar del pecado que les ha alejado de Dios y del propósito perfecto que tiene para cada uno. <br /><br />
+                    A medida que el adolescente crece en el ministerio realizamos acompañamiento personalizado en las diferentes necesidades que cada uno tiene.</p>
+                </div>
+            </div>
+
         </>
 
     )
