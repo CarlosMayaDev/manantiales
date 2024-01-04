00bd90ba280b172211407c5c058f1457a2fa1120
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2024-01-04 10:59:16 -0500

    Landing details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,19 @@ import styles from "./Card.module.css"
 import { NavLink } from "react-router-dom";
 
 function Card({ image, title, description }) {
+  const cardStyle = {
+    backgroundImage: `url(${image})`,
+  };
     return (
       <>
         <div className={styles.card}>
-          <img
-            className={styles.image}
-            src={image}        
-            alt="Card Image"
-          ></img>
-
+        <NavLink to="/ministerios" style={{ textDecoration: 'none' }}>
+        <div className={`${styles.card} ${styles.backgroundImage}`} style={cardStyle}></div>
             <div className={styles.content}>
-            <NavLink to="/ministerios" style={{ textDecoration: 'none' }}><h2 className={styles.h2}>{title}</h2></NavLink>
+              <h2 className={styles.h2}>{title}</h2>
               <p className={styles.p}>{description}</p>
             </div>
+            </NavLink>
         </div>
       </>
     );
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import imagen1 from "../assets/273541995_5604577989571712_5890615490078742919_n.jpg";
 import imagen2 from "../assets/2TO5NDkdQVAq63fc8XWeo0nyHhbHfrdhWUpN3YLnjCg=_plaintext_638363737807369320.jpg";
+import imagen3 from "../assets/LOGOCONQUISTADORES.PNG";
 import Card from './Card';
 import styles from "./Carousel.module.css";
 
@@ -14,10 +15,10 @@ function ImageCarousel() {
         interval={3500}
         showStatus={false}
         showThumbs={false}
-        transitionTime={1000}
+        transitionTime={3500}
       >
         <div className={styles.cardContainer}>
-          <Card image={imagen1} title="Conquistadores del Reino" description="Ministerio Infantil"/>
+          <Card image={imagen3} title="Conquistadores del Reino" description="Ministerio Infantil"/>
           <Card image={imagen2} title="ManantialEs Familia" description="Ministerio Juvenil"/>
           <Card image={imagen1} title="Adoración" description="Ministerio de Adoración"/>
         </div>
